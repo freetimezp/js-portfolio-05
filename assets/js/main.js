18d6f219d1c9a8fbf3683c8ed0d553607dee3da4
freetimezp <kraftukrnet@ukr.net>
2022-10-10 22:01:43 +0300

    07. js-portfolio-05. Skills section as tabs func.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,24 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContent = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove('skills__active');
+        });
+        target.classList.add('skills__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('skills__active');
+        });
+        tab.classList.add('skills__active');
+    });
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
