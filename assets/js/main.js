1f5a7579f186b6a5dde10ee32cd30ba7708eece1
freetimezp <kraftukrnet@ukr.net>
2022-10-11 19:31:25 +0300

    11. js-portfolio-05. Work popup func.

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,24 @@ function activeWork() {
 linkWork.forEach(item => item.addEventListener('click', activeWork));
 
 /*===== Work Popup =====*/
+document.addEventListener('click', (e) => {
+   if(e.target.classList.contains("work__button")) {
+       togglePortfolioPopup();
+       portfolioItemDetails(e.target.parentElement);
+   }
+});
 
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
 
@@ -67,3 +84,28 @@ linkWork.forEach(item => item.addEventListener('click', activeWork));
 
 
 /*=============== SHOW SCROLL UP ===============*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
