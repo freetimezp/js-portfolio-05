c0b869a0063638dd7f5a9b955460ef4a07713e1e
freetimezp <kraftukrnet@ukr.net>
2022-10-12 18:29:15 +0300

    17. js-portfolio-05. Responsive view. Show, hide main menu.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 /*=============== SHOW SIDEBAR ===============*/
-
+const navMenu = document.getElementById("sidebar");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-sidebar');
+    });
+}
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-sidebar');
+    });
+}
 
 /*=============== SKILLS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]');
